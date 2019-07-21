05de694b3bb304ef91daec01d1e29caa05937f4f
yxforever <1192054016@qq.com>
2019-07-21 15:47:35 +0800

    公英单位转换

--- code/Pytrain/分支语句/intocm.py
+++ code/Pytrain/分支语句/intocm.py
@@ -2,7 +2,9 @@
 公英制单位转换
 """
 
+
 value = float(input('请输入长度：'))
+
 unit = input('请输入单位（in/英寸，cm/厘米）：')
 
 if unit == 'in' or unit == '英寸':
@@ -11,4 +13,5 @@ elif unit == 'cm' or unit == '厘米':
     print('%f厘米 = %f英寸'% (value,value /2.54))
 else:
     print('请输入正确的单位。')
+
 
