79fdc3b7589213497990520d471f5080bf2ec244
yxforever <1192054016@qq.com>
2019-07-21 15:55:12 +0800

    掷骰子

---
+++ code/Pytrain/分支语句/掷骰子.py
@@ -0,0 +1,18 @@
+from random import randint
+
+face = randint(1,6)
+
+if face == 1:
+    result = '1'
+elif face == 2:
+    result = '2'
+elif face == 3:
+    result = '3'
+elif face == 4:
+    result = '4'
+elif face == 5:
+    result = '5'
+elif face == 6:
+    result = '6'
+
+print(result)
